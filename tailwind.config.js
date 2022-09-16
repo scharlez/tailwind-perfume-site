4ae77093143a7ad594083327ed3f115897e862dc
scharlez <stevencharlez@gmail.com>
2022-09-16 15:33:59 -0700

    Remodeled with basic styling and alignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
-		
+		extend: {
+			colors: {
+				primary: {
+					'dark-cyan': 'hsl(158, 36%, 37%)',
+					'cream': 'hsl(30, 38%, 92%)'
+				},
+				neutral: {
+					'very-dark-blue': 'hsl(212, 21%, 14%)',
+					'dark-grayish-blue': 'hsl(228, 12%, 48%)', 
+			
+				}
+			}
+		},
+			
+
+
+
 	},
 	plugins: [],
 };
